785297bda9376eff11277a926221c71e51dc7899
Osvetamp4 <82180979+Osvetamp4@users.noreply.github.com>
2025-02-08 17:02:57 -0500

    Added api route logEnergyMood(Aarushi)
    Added the api post route to log EnergyMood that Aarushi made to app.js

--- backend/app.js
+++ backend/app.js
@@ -135,6 +135,32 @@ app.post("/api/logThrowaway", async (request,response) =>{
 })
 
 
+app.post("/api/logEnergyMood", async (request, response)=> {
+    const requestDoc = request.body.energy_mood
+
+    const requestEnergy = requestDoc.energy
+    const requestMood = requestDoc.mood
+    const requestDate = requestDoc.date
+    const requestUser = requestDoc.username
+
+    const filter = {username:requestUser}
+
+    const package = {
+        date:requestDate,
+        mood:requestMood,
+        energy:requestEnergy
+    }
+
+    const updateDoc = {
+        $push: {
+            energy_mood_timeline:package
+        }
+    }
+
+    const result = await collection.updateOne(filter, updateDoc)
+})
+
+
 // getting the vape brands inputted by that one specific user
 app.get("/api/getvapebrands", async (request,response)=>{
     const requestUser = request.body.username
