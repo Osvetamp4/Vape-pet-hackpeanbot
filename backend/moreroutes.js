efdca1f571b6a8b7c45997dea708d88b5506e504
athejaswi <aarushi.s.t1@gmail.com>
2025-02-08 16:53:43 -0500

    fixing logEnergyMood post route

--- backend/moreroutes.js
+++ backend/moreroutes.js
@@ -1,5 +1,24 @@
-app.get("/api/getmoods",async (request, response)=> {
-    const requestUser = request.body.username
-    const query = await collection.findOne({username:requestUser})
-    const em = query.energy_mood_timeline
+app.post("/api/logEnergyMood", async (request, response)=> {
+    const requestDoc = request.body.energy_mood
+
+    const requestEnergy = requestDoc.energy
+    const requestMood = requestDoc.mood
+    const requestDate = requestDoc.date
+    const requestUser = requestDoc.username
+
+    const filter = {username:requestUser}
+
+    const package = {
+        logDate:requestDate,
+        logMood:requestMood,
+        logEnergy:requestEnergy
+    }
+
+    const updateDoc = {
+        $push: {
+            energy_mood_timeline:package
+        }
+    }
+
+    const result = await collection.updateOne(filter, updateDoc)
 })
